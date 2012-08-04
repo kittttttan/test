6ffbedc88ee91d06b78afff66a6773b847c01032
kittttttan <x@x>
2012-08-04 16:06:03 +0900

    r

--- c/cpp11/main.cc
+++ c/cpp11/main.cc
@@ -3,12 +3,15 @@
  * http://blogs.msdn.com/b/vcblog/archive/2011/09/12/10209291.aspx
  *
  * C++11
- * http://en.wikipedia.org/wiki/C%2B%2B11#Right_angle_bracket
+ * http://en.wikipedia.org/wiki/C%2B%2B11
  */
 
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
+#include <initializer_list>
+#include <regex>
+#include <random>
 
 const int elementCount = 9;
 
@@ -41,15 +44,28 @@ class X {
 
 // override and final
 struct Base {
+    //Base() = default;
+    //Base(const Base&) = delete;
     virtual void some_func(int);
     //virtual void final_func() final;
 };
- 
+
 struct Derived : Base {
     virtual void some_func(int) override;
     //virtual void some_func(float) override;
     //void final_func();
 };
+
+class InitList {
+public:
+    InitList() { }
+    InitList(std::initializer_list<int> list) {
+        std::for_each(list.begin(), list.end(), [](int n) { printf("%d\n", n); });
+    }
+};
+void func_init_list(std::initializer_list<int> list) {
+    std::for_each(list.begin(), list.end(), [](int n) { printf("%d\n", n); });
+}
 
 int main() {
     printf("** Rvalue references **\n");
@@ -109,7 +125,8 @@ int main() {
             printf("Local\n");
         }
     };
-    
+
+    //Local lo{1};
     //printf("sizeof(Local::value) = %d\n", sizeof(Local::value));
     const std::vector<Local> vec(1, Local());
     vec.front().print();
@@ -126,13 +143,48 @@ int main() {
     //printf("\n** Range-based for-loop **\n");
     //int my_array[] = {0,1,2,3,4,5,6};
     //for (int& x : my_array) {
-	//	++x;
-	//}
-	//for (int& x : my_array) {
-	//	printf("%d\n", x);
-	//}
+    //	++x;
+    //}
+    //for (int& x : my_array) {
+    //	printf("%d\n", x);
+    //}
+
+    //InitList list({1, 2, 3});
+    //func_init_list({1,2,3});
+
+    printf("\n** regex **\n");
+    std::regex re("\\d+");
+    std::match_results<const char *> results;
+    char* src = "mm123456nn7";
+    if (!std::regex_search(src, results, re, std::regex_constants::match_default)) {
+        return 1;
+    }
+
+    printf("prefix: %s\n", results.prefix().str().c_str());
+    printf("suffix: %s\n", results.suffix().str().c_str());
+    printf("posision: %d\n", results.position());
+    printf("str: %s\n", results.str().c_str());
+
+    for (auto it = results.begin(); it != results.end(); ++it) {
+        if (it->matched) {
+            printf("sub_match: %s\n", it->str().c_str());
+        }
+    }
+
+    std::regex re2("[^\\d]+");
+    std::string replaced = std::regex_replace(std::string(src), re2, std::string(""),
+        std::regex_constants::match_default);
+    printf("%s\n", replaced.c_str());
+
+    printf("\n** random **\n");
+    std::uniform_int_distribution<int> dist(0, 99);
+    std::mt19937 engine; // Mersenne twister MT19937
+    for (int i = 0; i < 7; ++i) {
+        int rand = dist(engine);
+        printf("%d\n", rand);
+    }
 
     system("pause");
 
-	return 0;
+    return 0;
 }
